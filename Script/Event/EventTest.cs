84ee76295319593c12f1cb3ad9f8e8bad62fb778
SaiTingHu <987947865@qq.com>
2023-11-27 16:41:53 +0800

    优化：同步主框架。

--- Script/Event/EventTest.cs
+++ Script/Event/EventTest.cs
@@ -9,30 +9,45 @@ namespace HT.Framework.Demo
     {
         private void Awake()
         {
-            //订阅3型事件
+            //订阅事件
             Main.m_Event.Subscribe<CustomEvent>(OnCustomEvent);
         }
 
         private void OnDestroy()
         {
-            //取消订阅3型事件
+            //取消订阅事件
             Main.m_Event.Unsubscribe<CustomEvent>(OnCustomEvent);
         }
 
         private void OnGUI()
         {
             GUILayout.BeginHorizontal();
+            if (GUILayout.Button("Throw CustomEvent (args)"))
+            {
+                //抛出事件（带参数）
+                Main.m_Event.Throw(Main.m_ReferencePool.Spawn<CustomEvent>().Fill(Random.Range(0, 100)));
+            }
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
             if (GUILayout.Button("Throw CustomEvent"))
             {
-                //抛出3型事件（订阅的事件类型必须与抛出的事件类型一致）
-                Main.m_Event.Throw(Main.m_ReferencePool.Spawn<CustomEvent>().Fill(Random.Range(0, 100)));
+                //抛出事件
+                Main.m_Event.Throw<CustomEvent>();
             }
             GUILayout.EndHorizontal();
         }
 
         private void OnCustomEvent(EventHandlerBase eventHandler)
         {
-            Log.Info("接收到CustomEvent事件，参数：" + (eventHandler as CustomEvent).Args);
+            if (eventHandler != null)
+            {
+                Log.Info("接收到CustomEvent事件，参数：" + (eventHandler as CustomEvent).Args);
+            }
+            else
+            {
+                Log.Info("接收到CustomEvent事件，无参数。");
+            }
         }
     }
 }
