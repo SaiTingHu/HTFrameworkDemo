5acf6304ccd3d18260d18f17d01bed97b20397a1
SaiTingHu <987947865@qq.com>
2024-05-29 14:45:40 +0800

    新增：Instruction指令系统的演示场景。

---
+++ Script/Instruction/InstructionTest.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace HT.Framework.Demo
+{
+    /// <summary>
+    /// 指令测试
+    /// </summary>
+    public class InstructionTest : MonoBehaviour
+    {
+        [Label("指令代码")] public InstructionAgent Agent;
+    }
+}
